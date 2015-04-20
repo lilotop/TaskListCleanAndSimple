5a8cbd513c9eed4704b1d9adaa603c25c6f15fc8
shaiazulay <shai.azulay@gmail.com>
2015-04-20 19:32:32 +0300

    Merge branch 'master' of https://github.com/shaiazulay/TaskListCleanAndSimple
    Conflicts:
    	.gitignore
    	README.md

--- app/Controllers.js
+++ app/Controllers.js
@@ -1,5 +1,14 @@
 angular.module("tasklist", [])
     .controller("taskController", function($scope) {
-        $scope.tasks = {};
-        $scope.name = "lilo";
-    });
+        $scope.tasks = [];
+        $scope.addTask = function() {
+            if (event.keyCode == 13 && $scope.newTask) {
+                $scope.tasks.push($scope.newTask);
+                $scope.newTask = "";
+            }
+        }
+        $scope.deleteTask = function()
+        {
+
+        }
+    })
